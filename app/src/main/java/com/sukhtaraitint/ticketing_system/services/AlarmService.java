0c2f475883134fdb0af8e4bf061448b78dc3cd6d
al-amin-tbd <al-amin@technobd.com>
2021-12-22 08:31:50 +0600

    Source code uupdated.

--- app/src/main/java/com/sukhtaraitint/ticketing_system/services/AlarmService.java
+++ app/src/main/java/com/sukhtaraitint/ticketing_system/services/AlarmService.java
@@ -17,42 +17,28 @@ import android.os.IBinder;
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
+import com.sukhtaraitint.ticketing_system.R;
+import com.sukhtaraitint.ticketing_system.ReportActivity;
+import com.sukhtaraitint.ticketing_system.utils.Variables;
+
 public class AlarmService extends Service {
     private MediaPlayer mediaPlayer;
     //    private Vibrator vibrator;
-
-    private SharedPrefData sharedPrefData;
 
     public static final String ALARM_CHANNEL_ID = "ALARM_SERVICE_CHANNEL_NEW";
 
     @Override
     public void onCreate() {
-        sharedPrefData = new SharedPrefData(getApplicationContext());
         Variables.PRAYER_ALARM_ONGOING = true;
-        try{
-            if(SuraListFragment.getInstance() != null){
-                if(SuraListFragment.getInstance().suraListHandler != null){
-                    SuraListFragment.getInstance().suraListHandler.adhanAlarmUpdateUI();
-                }
-            }
-        }catch (Exception exception){
-            exception.printStackTrace();
-        }
         super.onCreate();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-
-        sharedPrefData = new SharedPrefData(getApplicationContext());
         Variables.PRAYER_ALARM_ONGOING = true;
 
         try{
-            if(SuraListFragment.getInstance() != null){
-                SuraListFragment.getInstance().suraListHandler.adhanAlarmUpdateUI();
-            }
-
-            Intent notificationIntent = new Intent(this, PrayerAlarmActivity.class);
+            Intent notificationIntent = new Intent(this, ReportActivity.class);
             int alarmID = intent.getIntExtra("requestCode", 0);
             String PrayerName = intent.getStringExtra("PrayerName");
             String AlarmType = intent.getStringExtra("AlarmType");
@@ -68,7 +54,7 @@ public class AlarmService extends Service {
             AudioManager am;
             if(AlarmType != null && PrayerName != null){
                 try{
-                    if(AlarmType.equals("notification")){
+                    /*if(AlarmType.equals("notification")){
                         mediaPlayer = new MediaPlayer();
                         am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
                         am.setStreamVolume(AudioManager.STREAM_RING, am.getStreamMaxVolume(AudioManager.STREAM_RING), 0);
@@ -88,7 +74,7 @@ public class AlarmService extends Service {
                             mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.adhan_trimmed);
                             mediaPlayer.start();
                         }
-                    }
+                    }*/
                     String message ="Close_Service";
 
                     if(mediaPlayer != null){
@@ -96,9 +82,6 @@ public class AlarmService extends Service {
                             @Override
                             public void onCompletion(MediaPlayer mp) {
                                 Variables.PRAYER_ALARM_ONGOING = false;
-                                if(SuraListFragment.getInstance() != null){
-                                    SuraListFragment.getInstance().suraListHandler.adhanAlarmUpdateUI();
-                                }
 
                                 stopForeground(true);
                                 if(mediaPlayer != null){
@@ -121,7 +104,7 @@ public class AlarmService extends Service {
                     notification = new NotificationCompat.Builder(this, ALARM_CHANNEL_ID)
                             .setContentTitle(PrayerName)
                             .setContentText(alarmTitle)
-                            .setSmallIcon(R.drawable.ic_alarm_black_24dp)
+                            .setSmallIcon(R.drawable.ic_launcher_foreground)
                             .setContentIntent(pendingIntent)
                             .setColor(Color.RED)
                             .setAutoCancel(true)
@@ -138,7 +121,7 @@ public class AlarmService extends Service {
                     notification = new NotificationCompat.Builder(this, ALARM_CHANNEL_ID)
                             .setContentTitle(PrayerName)
                             .setContentText(alarmTitle)
-                            .setSmallIcon(R.drawable.ic_alarm_black_24dp)
+                            .setSmallIcon(R.drawable.ic_launcher_foreground)
                             .setContentIntent(pendingIntent)
                             .setAutoCancel(true)
                             .setGroup(Variables.NOTIFICATION_GROUP_ID)
